cfb3be264988e25c505fc3e7044ffe121999ab2e
Piero Maldonado <pieromaldonado16@gmail.com>
2023-05-18 16:32:52 -0500

    user email y name en el home

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,13 +1,13 @@
 module.exports = {
-    index: function (req, res) {
-      res.render('index');
-    },
-    login: function (req, res) {
-      res.oidc.login({
-        returnTo: '/home'
-      });
-    },
-    home: function (req, res) {
-      res.render('home');
-    }
-  };
+  index: function (req, res) {
+    res.render('index');
+  },
+  login: function (req, res) {
+    res.oidc.login({
+      returnTo: '/home'
+    });
+  },
+  home: function (req, res) {
+    res.render('home', { user: req.oidc.user });
+  }
+};
