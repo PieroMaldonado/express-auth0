ea0516c282897370aaefe6382541bf1dc4208355
Piero Maldonado <63681717+PieroMaldonado@users.noreply.github.com>
2023-06-23 15:51:08 -0500

    Update indexController.js

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -30,24 +30,34 @@ module.exports = {
 
   encryptData: async function (req, res) {
 
+    // Se convierte la cédula recibida en un objeto Buffer
     const plaintextBuffer = Buffer.from(req.body.cedula);
-    
+
+    // Se realiza la encriptación de la cédula con cloud KMS
     const [encryptResponse] = await client.encrypt({
       name: keyName,
       plaintext: plaintextBuffer,
     });
-  
+
+    // Se obtiene el texto cifrado de la respuesta de encriptación
     const ciphertext = encryptResponse.ciphertext;
+
+    // Se imprime por consola la cédula original y su versión encriptada
     console.log("Cédula: ",req.body.cedula)
     console.log(`Cédula encriptada: ${ciphertext.toString('base64')}`);
-    
+
+    // Se construye la URL para la solicitud a la API externa con la cédula encriptada como parámetro
     const url = `https://crudempresasapi.azurewebsites.net/api/controladorAPI/validarCedula?cedula=${ciphertext.toString('base64')}`;
 
     try {
+       // Se realiza la solicitud a la API externa
       const response = await fetch(url);
+      // Se obtiene el cuerpo de la respuesta como texto
       const data = await response.text();
+      // Se envía la respuesta al cliente
       res.send(data);
     } catch (error) {
+      // Si ocurre un error, se imprime en la consola y se envía una respuesta de error al cliente
       console.error(error);
       res.status(500).send('Error al realizar la solicitud a la API externa');
     }
